aa7f19f0c3f186779ac94c078620272208cd5192
HSC <55527802+huoshengchiou@users.noreply.github.com>
2021-10-25 16:28:28 +0800

    [adjust]

---
+++ src/app/Task.ts
@@ -0,0 +1,7 @@
+//定義Task schema
+export interface Task {
+  id?: number;
+  text: string;
+  day: string;
+  reminder: boolean;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SquareComponent } from './square/square.component';
-import { BoardComponent } from './board/board.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SquareComponent,
-    BoardComponent
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
